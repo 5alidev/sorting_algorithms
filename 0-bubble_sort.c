511442ef95028ce168cca97e65e833f865a93b54
5alidev <khalidbenhamou.dev@outlook.com>
2024-02-15 05:00:13 -0800

    optimize code

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,12 +11,13 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int temp;
+	size_t i = 0, j;
+	int temp, swapped = 0;
 
-	for (i = 0; i < size; i++)
+	do
 	{
-		for (j = 0; j < size - 1; j++)
+		swapped = 0;
+		for (j = 0; j < (size - 1 - i); j++)
 		{
 			if (array[j] > array[j + 1])
 			{
@@ -24,7 +25,9 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				print_array(array, size);
+				swapped = 1;
 			}
 		}
-	}
+		i++;
+	} while (swapped == 1);
 }
